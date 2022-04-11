b9b652d0a47ac952fd1c3b75468347f768642710
Sachin Bhatnagar <mailme@sachinbhatnagar.com>
2022-04-11 13:33:14 +0530

    Peer Reviewed - 11 Apr 2022

--- index.js
+++ index.js
@@ -1,68 +1,65 @@
+import { monsters } from './monsters.js';
 
-import {monsters} from './monsters.js';
-console.log(monsters);
-for(let monsterDiv of monsters){
-    showMonster(monsterDiv);
+const monsterContainer = document.querySelector('.monsters');
+const monsterFilter = document.querySelector('#search-monster');
 
+function populateMonsters(content) {
+  // Clear out existing content
+  monsterContainer.innerHTML = '';
+  // Usde showMonster to generate an Array of DOMNodes
+  let createMonsters = content.map((el) => showMonster(el));
+  // Append the DOMNodes in one DOM transaction
+  monsterContainer.append(...createMonsters);
 }
 
-function showMonster(monsterDiv){
-    const monster = document.createElement('div');
-    monster.className = 'monster';
-    const img= document.createElement('img');
-    img.src = `https://robohash.org/${monsterDiv.id}?set=set2`;
-    img.alt = monsterDiv.name ;
-    const mname = document.createElement('p');
-    mname.className = 'name' ;
-    mname.innerText = monsterDiv.name;
-    const email = document.createElement('p');
-    email.className = 'email'
-    email.innerText = monsterDiv.email;
-    monster.append(img,mname,email);
-    document.querySelector('.monsters').append(monster);
-    console.log(monster);
-}
+function showMonster(monsterDiv) {
+  const monster = document.createElement('div');
+  monster.className = 'monster';
 
+  const img = document.createElement('img');
+  img.src = `https://robohash.org/${monsterDiv.id}?set=set2`;
+  img.alt = monsterDiv.name;
+
+  const mname = document.createElement('p');
+  mname.className = 'name';
+  mname.innerText = monsterDiv.name;
+
+  const email = document.createElement('p');
+  email.className = 'email';
+  email.innerText = monsterDiv.email;
+  monster.append(img, mname, email);
+
+  return monster;
+}
 
 function showMonsterNotFound() {
-    const notFound= document.createElement('div');
-    notFound.className='p-5 not-found';
-    // notFound.style.display='none';
-    const span = document.createElement('span');
-    span.innerText='404';
-    const h1 = document.createElement('h1')
-    h1.innerText='🧟‍♂️ No Monster Found 🧟‍♂️' ;
-    notFound.append(span,h1);
-    document.querySelector('.monsters').append(notFound);
-    console.log(notFound);
-
-
+  const notFound = document.createElement('div');
+  notFound.className = 'p-5 not-found';
+  const span = document.createElement('span');
+  span.innerText = '404';
+  const h1 = document.createElement('h1');
+  h1.innerText = '🧟‍♂️ No Monster Found 🧟‍♂️';
+  notFound.append(span, h1);
+  return notFound;
 }
-showMonsterNotFound()
 
-document.querySelector('#search-monster').addEventListener('keyup', function(e) {
-    const keyword= e.target.value.toLowerCase();
-    const findMonster = document.querySelectorAll('.monster');
-    let notFound = true;
+monsterFilter.addEventListener('input', function (e) {
+  if (e.target.value !== '') {
+    const keyword = e.target.value.toLowerCase();
 
+    const filtered = monsters.filter((el) => {
+      if (el.name.toLowerCase().includes(keyword) || el.email.toLowerCase().includes(keyword)) {
+        return true;
+      }
+    });
 
-    for(let monster of findMonster){
-        const name =monster.children[1].innerText.toLowerCase();
-        const email = monster.children[2].innerText.toLowerCase();
-        if(name.includes(keyword) || email.includes(keyword)){
-            monster.style.display = 'block';
-            notFound=false;
-
-        }
-        else{
-            monster.style.display = 'none';
-        }
+    if (filtered.length === 0) {
+      monsterContainer.innerHTML = '';
+      monsterContainer.append(showMonsterNotFound());
+    } else {
+      populateMonsters(filtered);
     }
-    if(notFound){
-        document.querySelector('.not-found').style.display ='block' ;
-    }
-    else{
-        document.querySelector('.not-found').style.display ='none' ;
-    }
-})
+  }
+});
 
+populateMonsters(monsters);
